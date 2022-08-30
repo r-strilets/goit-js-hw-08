d2b1d616262dee897c7839e3babff9b9325f9fb1
r-strilets <55804526+r-strilets@users.noreply.github.com>
2022-08-30 16:52:50 +0300

    add task_1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,27 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+
+function createGallery(arrayOfImages) {
+  const newGalleryItems = arrayOfImages
+    .map(({ preview, original, description }) => {
+      return `
+    <a class="gallery__item" href="${original}">
+    <img class="gallery__image" src="${preview}" alt="${description}" />
+  </a>
+ `;
+    })
+    .join('');
+  return newGalleryItems;
+}
+gallery.innerHTML = createGallery(galleryItems);
+
+let galleryBox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+galleryBox.on('show.simplelightbox', function () {});
