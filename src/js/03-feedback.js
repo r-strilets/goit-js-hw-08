24c875d10c2f27eba0b4738438b86eaeca59beff
r-strilets <str.komar@gmail.com>
2022-09-01 00:32:44 +0300

    add task_3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,25 @@
+const throttle = require('lodash.throttle');
+const feedbackForm = document.querySelector('.feedback-form');
 
+const FEEDBACK_KEY = 'feedback-form-state';
+const formData = {};
+
+feedbackForm.addEventListener('input', throttle(newFormData, 500));
+function newFormData(e) {
+  formData[e.target.name] = e.target.value;
+  localStorage.setItem(FEEDBACK_KEY, JSON.stringify(formData));
+}
+
+feedbackForm.addEventListener('submit', onClickFormSubmit);
+function onClickFormSubmit(e) {
+  localStorage.clear();
+  e.target.reset();
+}
+try {
+  const newData = JSON.parse(localStorage.getItem(FEEDBACK_KEY));
+
+  feedbackForm.email.value = newData.email;
+  feedbackForm.message.value = newData.message;
+} catch (e) {
+  console.log('no data in locale storage');
+}
