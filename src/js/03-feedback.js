18d3e4eb3427368e6c548eca57e7473f8e009626
r-strilets <str.komar@gmail.com>
2022-09-01 19:11:54 +0300

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -2,30 +2,36 @@ const throttle = require('lodash.throttle');
 const feedbackForm = document.querySelector('.feedback-form');
 
 const FEEDBACK_KEY = 'feedback-form-state';
-const formData = {};
-let oldData = {};
+let formData = JSON.parse(localStorage.getItem(FEEDBACK_KEY)) || {};
+// let oldData = {};
+console.log(formData);
+feedbackForm.email.value = formData.email ?? '';
+feedbackForm.message.value = formData.message ?? '';
 
 feedbackForm.addEventListener('input', throttle(newFormData, 500));
 function newFormData(e) {
   formData[e.target.name] = e.target.value;
-
+  if (!e.target.elements.email.value) {
+    document.querySelector('button').disabled = true;
+  }
   localStorage.setItem(FEEDBACK_KEY, JSON.stringify(formData));
 }
+document
+  .querySelector('button')
+  // try {
+  //   oldData =
 
-try {
-  oldData = JSON.parse(localStorage.getItem(FEEDBACK_KEY));
-
-  feedbackForm.email.value = oldData.email;
-  feedbackForm.message.value = oldData.message;
-} catch (e) {
-  console.log('no data in locale storage');
-}
-feedbackForm.addEventListener('submit', onClickFormSubmit);
+  //   feedbackForm.email.value = oldData.email;
+  //   feedbackForm.message.value = oldData.message;
+  // } catch (e) {
+  //   console.log('no data in locale storage');
+  // }
+  .feedbackForm.addEventListener('submit', onClickFormSubmit);
 function onClickFormSubmit(e) {
   e.preventDefault();
   e.target.reset();
-  console.log(oldData);
-  oldData = '';
+  console.log(formData);
+  formData = {};
 
   localStorage.removeItem(FEEDBACK_KEY);
 }
