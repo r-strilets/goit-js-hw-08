e7f80d46e6a8bfc4650fdd422938487dc250adc3
r-strilets <str.komar@gmail.com>
2022-09-01 19:51:21 +0300

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,19 +1,20 @@
 const throttle = require('lodash.throttle');
 const feedbackForm = document.querySelector('.feedback-form');
-
+const isButtonActive = document.querySelector('button');
 const FEEDBACK_KEY = 'feedback-form-state';
 let formData = JSON.parse(localStorage.getItem(FEEDBACK_KEY)) || {};
-// let oldData = {};
-console.log(formData);
+
 feedbackForm.email.value = formData.email ?? '';
 feedbackForm.message.value = formData.message ?? '';
+
+isButtonActive.disabled = true;
 
 feedbackForm.addEventListener('input', throttle(newFormData, 500));
 function newFormData(e) {
   formData[e.target.name] = e.target.value;
-  if (!e.target.elements.email.value) {
-    document.querySelector('button').disabled = true;
-  }
+
+  document.querySelector('button').disabled = false;
+
   localStorage.setItem(FEEDBACK_KEY, JSON.stringify(formData));
 }
 
@@ -23,6 +24,6 @@ function onClickFormSubmit(e) {
   e.target.reset();
   console.log(formData);
   formData = {};
-
   localStorage.removeItem(FEEDBACK_KEY);
+  document.querySelector('button').disabled = true;
 }
