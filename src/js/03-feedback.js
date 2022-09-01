02722505e09b91506d3d716625a8c1ac8408e5bc
r-strilets <str.komar@gmail.com>
2022-09-01 10:43:33 +0300

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -7,19 +7,23 @@ const formData = {};
 feedbackForm.addEventListener('input', throttle(newFormData, 500));
 function newFormData(e) {
   formData[e.target.name] = e.target.value;
+
   localStorage.setItem(FEEDBACK_KEY, JSON.stringify(formData));
 }
 
-feedbackForm.addEventListener('submit', onClickFormSubmit);
+feedbackForm.addEventListener('submit', onClickFormSubmit, { once: true });
+
 function onClickFormSubmit(e) {
-  localStorage.clear();
+  e.preventDefault();
   e.target.reset();
+  console.log(formData);
+  localStorage.clear();
 }
 try {
-  const newData = JSON.parse(localStorage.getItem(FEEDBACK_KEY));
+  const oldData = JSON.parse(localStorage.getItem(FEEDBACK_KEY));
 
-  feedbackForm.email.value = newData.email;
-  feedbackForm.message.value = newData.message;
+  feedbackForm.email.value = oldData.email;
+  feedbackForm.message.value = oldData.message;
 } catch (e) {
   console.log('no data in locale storage');
 }
